e3758d10d9973b93c9eb201c401da050237ec9b9
ZEROUKI <zerouki@mac.home>
2025-08-19 11:35:02 +0200

    first seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts " nettoyer user"
+User.destroy_all
+
+puts " creation de seeds"
+user1 = User.create!(name: 'Jonathan', email: "Jonathan@gmail.com",
+   password: "password", height:180, weight: 75.3, gender: "homme", age: 24)
+user2 = User.create!(name: 'Charles', email: "Charles@gmail.com", password: "password4",
+   height: 183, weight: 79.3, gender: "homme", age: 27)
+
+
+puts "reation d'objectif"
+
+Objective.create!(name: "perte de poids", description:"perte de poids intensive pour remise en forme", user:user1)
+Objective.create!(name: "prise de masse", description:"prise de masse intensive", user:user2)
+
+puts "seeds done"

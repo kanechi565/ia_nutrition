e369ff8a75f678bb6de6d1e2edb321a1e1a1c732
exec zsh <jonathanfournier565@gmail.com>
2025-08-19 11:48:46 +0200

    create new objective

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -1,2 +1,19 @@
 class ObjectivesController < ApplicationController
+  def new
+    @objective = Objective.new
+  end
+
+  def create
+    @objective = Objective.new(objective_params)
+    if @objective.save!
+      redirect_to objectives_path, notice: "objectif crée"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+private
+
+  def objective_params
+    params.require(:objective).permit(:name, :description)
+  end
 end
